fb3939731f10dcc497202ed85a5b9c96505f44dc
NickBrown4211 <100061254+NickBrown4211@users.noreply.github.com>
2022-04-03 07:16:08 -0400

    Create jags_place_overlay.user.js

---
+++ jags_place_overlay.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Jacksonville Jaguars Place
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Jags
+// @author       Purpose
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/kylomorales/rocketleagueplace/raw/main/egg_people_unite_overlay.user.js
+// @downloadURL  https://github.com/kylomorales/rocketleagueplace/raw/main/egg_people_unite_overlay.user.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/Nickbrown4211/JagsPlace/raw/main/new_overlay14.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
